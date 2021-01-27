295bc9b92d91ce6aca4dc983a44eb19da3de553c
dbstjr5517 <dbstjr5517@naver.com>
2021-01-27 13:04:16 +0900

    1/27 1:04
    p.200

--- Source/UE4_CPP/Private/ABPawn.cpp
+++ Source/UE4_CPP/Private/ABPawn.cpp
@@ -32,19 +32,21 @@ AABPawn::AABPawn()
 		Mesh->SetSkeletalMesh(SK_CARDBOARD.Object);
 	}
 
+	Mesh->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+
+	static ConstructorHelpers::FClassFinder<UAnimInstance> WARRIOR_ANIM(TEXT("/Game/InfinityBladeWarriors/Character/CompleteCharacters/WarriorAnimBluePrint_C"));
+
+	if(WARRIOR_ANIM.Succeeded())
+	{
+		Mesh->SetAnimInstanceClass(WARRIOR_ANIM.Class);
+	}
+
 }
 
 // Called when the game starts or when spawned
 void AABPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	Mesh->SetAnimationMode(EAnimationMode::AnimationSingleNode);
-	UAnimationAsset* AnimAsset = LoadObject<UAnimationAsset>(nullptr, TEXT("/Game/InfinityBladeWarriors/Character/CompleteCharacters/WarriorRun.WarriorRun"));
-
-	if(AnimAsset != nullptr)
-	{
-		Mesh->PlayAnimation(AnimAsset, true);
-	}
 	
 }
 
---
+++ Source/UE4_CPP/Public/ABCharacter.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UE4_CPP/UE4_CPP.h"
+#include "GameFramework/Character.h"
+#include "ABCharacter.generated.h"
+
+UCLASS()
+class UE4_CPP_API AABCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AABCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	enum class EControlMode
+	{
+		GTA,
+		DIABLO
+	};
+
+	void SetControlMode(EControlMode NewControlMode);
+	EControlMode CurrentControlMode = EControlMode::GTA;
+	FVector DirectionToMove = FVector::ZeroVector;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	UCameraComponent* Camera;
+
+private:
+	void UpDown(float NewAxisValue);
+	void LeftRight(float NewAxisValue);
+	void LookUp(float NewAxisValue);
+	void Turn(float NewAxisValue);
+
+	void ViewChange();
+
+};
